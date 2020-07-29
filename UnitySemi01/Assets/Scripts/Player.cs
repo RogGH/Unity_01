501c0f4613dfef8b562ca166c80c9196e49d8dc9
Rog_GH <baikanhu1004@yahoo.co.jp>
2020-07-29 17:26:49 +0900

    animation add

--- UnitySemi01/Assets/Scripts/Player.cs
+++ UnitySemi01/Assets/Scripts/Player.cs
@@ -16,16 +16,22 @@ public class Player : MonoBehaviour
     private Rigidbody2D rigidbody2D;    // コンポーネント用変数
     private BoxCollider2D boxcollider2D;// コンポーネント用変数
     private Animator animator;          // コンポーネント用変数 
-    private AnimatorStateInfo animInfo;
 
+    private string nowAnimName = null;  // 現在のアニメーションの名前
+    private string oldAnimName = null;  // ひとつ前のアニメーションの名前
+    private float nowAnimCtr = 0;       // アニメーションの再生場所を保存しておく
+
+    // プライベート変数
     private bool isGrounded;            // 地上にいるかの判定
     private float inputLR = 0;          // 左右入力
     private bool inputJump = false;     // ジャンプ入力
     private float shotWait = 0;         // 発射間隔用
  
     // const変数
-    const float JUMPUP_CHECK_SPEED = 10.0f;   // ジャンプ上昇中チェック用
+    const float JUMPUP_CHECK_SPEED = 1.0f;   // ジャンプ上昇中チェック用
     const float SHOT_WAIT_DEFAULT = 0.2f;    // 発射アニメーション終了時間 
+    Vector3 SHOT_GROUND_OFS = new Vector3(30.0f, 36.0f, 0 );    // 地上での発射オフセット
+    Vector3 SHOT_JUMP_OFS = new Vector3(30.0f, 42.0f, 0);       // 空中での発射オフセット
 
     // Start is called before the first frame update
     void Start()
@@ -34,109 +40,31 @@ public class Player : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
         boxcollider2D = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
-        animInfo = animator.GetCurrentAnimatorStateInfo(0);
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        // 接地チェック
-        if (isGrounded)
-        {
-            // 地上
-            rigidbody2D.velocity.Set(rigidbody2D.velocity.x, 0);    // Ｙ軸速度を０に
-            animator.SetBool("JumpUp", false);                    // ジャンプ上昇をOFF
-            animator.SetBool("JumpDown", false);                  // ジャンプ下降をOFF 
-        }
-        else
-        {
-            // 空中
-            // Ｙ軸方向の速度で、上昇中か下降中かを設定する(０の時は下降設定)
-            bool isJumpUp = rigidbody2D.velocity.y > JUMPUP_CHECK_SPEED ? true : false;
-            bool isJumpDown = rigidbody2D.velocity.y <= 0f ? true : false;
-            // アニメーション設定
-            animator.SetBool("JumpUp", isJumpUp);
-            animator.SetBool("JumpDown", isJumpDown);
-        }
+        // 前処理
+        preSetup();
 
-        //着地していた時
-        if (isGrounded)
-        {
-            // スペースキー入力をチェック
-            if (Input.GetKeyDown("space"))
-            {
-                // ジャンプ入力設定
-                inputJump = true;
-                // 着地判定をfalse
-                isGrounded = false;
-                // ジャンプ上昇へ
-                animator.SetBool("JumpUp", true);
-                animator.SetBool("JumpDown", false);
-            }
-        }
+        // ジャンプ制御
+        jumpControl();
 
-        // 入力を取得（WASDor十字キー）
-        float inputX = Input.GetAxisRaw("Horizontal");
+        // 歩き制御
+        moveControl();
 
-        // 左右入力があったら
-        if (inputX != 0)
-        {
-            inputLR = inputX;
+        // ショット制御
+        shotControl();
 
-            // 反転処理（ｘのスケール値がー１になると反転するので、
-            // 入力方向が左（－１）の時は右の絵が反転して表示される
-            Vector2 temp = transform.localScale;
-            temp.x = inputX;
-            transform.localScale = temp;
+        // 後処理
+        postSetup();
+    }
 
-            if (isGrounded) { 
-                // 入力があるときは走り状態へ
-                animator.SetBool("Run", true);
-            }
-        }
-        else
-        {
-            inputLR = 0;
-            // 入力がないときは走り状態解除
-            animator.SetBool("Run", false);
-        }
-
-        // 発射待機をチェック
-        if (shotWait > 0)
-        {
-            // 発射待機時間を減算する
-            shotWait -= Time.deltaTime;
-            if (shotWait <= 0)
-            {
-                // 発射フラグをOFFに
-                animator.SetBool("Shot", false);
-            }
-        }
-
-        // Ｚキー入力で発射
-        if (Input.GetKeyDown(KeyCode.Z))
-        {       
-            Vector3 bootOffset = new Vector3(30.0f, 36.0f, 0);
-            // ＰＬが向いている方向にオフセットをずらす
-            bootOffset.x *= transform.localScale.x;
-            // 弾起動
-            Instantiate(shell1,
-                transform.position + bootOffset,
-                transform.rotation);
-
-            // 発射フラグをONに
-            animator.SetBool("Shot", true);
-            // 次弾発射までの待機時間を設定
-            shotWait = SHOT_WAIT_DEFAULT;
-
-            // 待機の時のみ撃つたび初期化してみる
-            if (false)
-            {
-                animator.ForceStateNormalizedTime(0);
-            }
-        }
-
-        Debug.Log(animator.GetCurrentAnimatorStateInfo(0).length);
+    private void LateUpdate()
+    {
+        // Updateの後で行われる処理
     }
 
 
@@ -203,6 +131,164 @@ public class Player : MonoBehaviour
         }
         return result;
     }
+
+    // 前処理
+    void preSetup()
+    {
+        // アニメーションの名前を取得
+        AnimatorClipInfo[]　currentClipInfo = this.animator.GetCurrentAnimatorClipInfo(0);
+        string clipName = currentClipInfo[0].clip.name;
+        // アニメーション変更があったとき
+        if (clipName != nowAnimName)
+        {
+            // 古いアニメーションを保存
+            oldAnimName = nowAnimName;
+            // 現在のアニメーションの名前を保存
+            nowAnimName = clipName;
+            // 発射アニメ制御
+            shotAnimControl();
+        }
+
+        // 接地チェック
+        if (isGrounded)
+        {
+            // 地上
+            rigidbody2D.velocity.Set(rigidbody2D.velocity.x, 0);    // Ｙ軸速度を０に
+            animator.SetBool("JumpUp", false);                    // ジャンプ上昇をOFF
+            animator.SetBool("JumpDown", false);                  // ジャンプ下降をOFF 
+        }
+        else
+        {
+            // 空中
+            // Ｙ軸方向の速度で、上昇中か下降中かを設定する(０の時は下降設定)
+            bool isJumpUp = rigidbody2D.velocity.y > JUMPUP_CHECK_SPEED ? true : false;
+            bool isJumpDown = rigidbody2D.velocity.y <= 0f ? true : false;
+            // アニメーション設定
+            animator.SetBool("JumpUp", isJumpUp);
+            animator.SetBool("JumpDown", isJumpDown);
+        }
+    }
+
+    // 後処理
+    void postSetup()
+    {
+        // 後処理
+        nowAnimCtr = animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+    // 発射アニメーション制御
+    void shotAnimControl()
+    {
+        // アニメーションの名前を取得
+        // アニメーション変更があったとき
+        // 前のアニメーションが歩きで現在が歩き発射の場合
+        if ((oldAnimName == "PLRun" && nowAnimName == "PLRunShot")
+        || (oldAnimName == "PLRunShot" && nowAnimName == "PLRun")
+            )
+        {
+            // アニメーターの再生位置を継続させてみる
+            animator.Play(nowAnimName, -1, nowAnimCtr);         // 引継ぎ
+        }
+    }
+
+    // ジャンプ制御
+    void jumpControl()
+    {
+        //着地していた時
+        if (isGrounded)
+        {
+            // スペースキー入力をチェック
+            if (Input.GetKeyDown("space"))
+            {
+                // ジャンプ入力設定
+                inputJump = true;
+                // 着地判定をfalse
+                isGrounded = false;
+                // ジャンプ上昇へ
+                animator.SetBool("JumpUp", true);
+                animator.SetBool("JumpDown", false);
+            }
+        }
+    }
+
+    // 移動制御
+    void moveControl()
+    {
+        // 入力を取得（WASDor十字キー）
+        float inputX = Input.GetAxisRaw("Horizontal");
+
+        // 左右入力があったら
+        if (inputX != 0)
+        {
+            inputLR = inputX;
+
+            // 反転処理（ｘのスケール値がー１になると反転するので、
+            // 入力方向が左（－１）の時は右の絵が反転して表示される
+            Vector2 temp = transform.localScale;
+            temp.x = inputX;
+            transform.localScale = temp;
+
+            if (isGrounded)
+            {
+                // 入力があるときは走り状態へ
+                animator.SetBool("Run", true);
+            }
+        }
+        else
+        {
+            inputLR = 0;
+            // 入力がないときは走り状態解除
+            animator.SetBool("Run", false);
+        }
+    }
+
+    // 発射制御
+    void shotControl()
+    {
+        // 発射待機をチェック
+        if (shotWait > 0)
+        {
+            // 発射待機時間を減算する
+            shotWait -= Time.deltaTime;
+            if (shotWait <= 0)
+            {
+                // 発射フラグをOFFに
+                animator.SetBool("Shot", false);
+            }
+        }
+
+        // Ｚキー入力で発射
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Vector3 bootOffset = isGrounded  ? SHOT_GROUND_OFS : SHOT_JUMP_OFS;
+            // ＰＬが向いている方向にオフセットをずらす
+            bootOffset.x *= transform.localScale.x;
+            // 弾起動
+            Instantiate(shell1,
+                transform.position + bootOffset,
+                transform.rotation);
+
+            // 発射フラグをONに
+            animator.SetBool("Shot", true);
+            // 次弾発射までの待機時間を設定
+            shotWait = SHOT_WAIT_DEFAULT;
+
+            // 待機の時のみ撃つたび初期化してみる
+            if (nowAnimName == "PLStayShot")
+            {
+                animator.Play(nowAnimName, -1, 0);         // 最初から再生
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
     void OnCollisionEnter2D(Collision2D other)
